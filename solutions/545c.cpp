74a0bb3bb687961be7c7a911fbd0d698753798c1
Ismail Ibragim <ismailibragim@Ismails-MacBook-Pro.local>
2025-12-28 19:54:10 +0000

    545c

---
+++ solutions/545c.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <set>
+#include <map>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <chrono>
+#include <random>
+#include <functional>
+#include <limits>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define INPUT_OUTPUT()                  \
+    freopen("input.txt", "r", stdin);   \
+    freopen("output.txt", "w", stdout); 
+#else
+#define INPUT_OUTPUT()                  \
+    while (false) {};
+#endif
+
+#define FAST_EXECUTION
+#ifdef FAST_EXECUTION
+  #pragma GCC optimize("O3")
+#endif
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define sc set<char>
+
+#define F first
+#define S second
+
+/* FUNCTIONS */
+#define f(i,s,e) for(auto i=s;i<e;i++)
+#define cf(i,s,e) for(auto i=s;i<=e;i++)
+#define rf(i,e,s) for(auto i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}\n"; }
+
+template <class T>
+void print_a(T &v, int n) { cout << "{"; for (int i=0; i<n; i++) cout << v[i] << ","; cout << "\b}\n"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define N (int) 1e6
+#define INF (ll) 4e18
+
+void solve() {
+    int i, n;
+    cin >> n;
+    vector<pii > v;
+    vector<char> trees (n);
+    f(i, 0, n) {
+        int x, h;
+        cin >> x >> h;
+        v.pb(make_pair(x, h));
+        trees[i] = 'o';
+    }
+    int result = 0;
+    f(i, 0, n) {
+        auto [x1, h_x1] = v[i];
+        if (i == 0) {
+            trees[i] = 'l';
+            result++;
+            continue;
+        }
+        // check left
+        auto [x2, h_x2] = v[i-1];
+        if (((trees[i-1] == 'l' || trees[i-1] == 'o') && x2 < x1 - h_x1) || 
+            (trees[i-1] == 'r' && x2 + h_x2 < x1 - h_x1)) {
+            trees[i] = 'l';
+            result++;
+            continue;
+        };
+        // check right
+        if (i == n-1) {
+            trees[i] = 'r';
+            result++;
+            continue;
+        }
+        auto [x3, h_x3] = v[i+1];
+        if (x1 + h_x1 < x3) {
+            trees[i] = 'r';
+            result++;
+            continue;
+        }
+    };
+    cout << result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    INPUT_OUTPUT()
+    solve();
+    return 0;
+}
